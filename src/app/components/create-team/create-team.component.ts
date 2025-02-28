750bd3cc899306d1b33b5a0413cb7e50b89944b6
Ricardo Ramirez <6ricardo6ramirez6@gmail.com>
2025-02-28 08:36:30 -0300

    -v 0.0.3 more interfaces created

--- src/app/components/create-team/create-team.component.ts
+++ src/app/components/create-team/create-team.component.ts
@@ -1,11 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-create-team',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './create-team.component.html',
   styleUrl: './create-team.component.scss'
 })
 export class CreateTeamComponent {
+
+
+  ttlDisplay:boolean = false;
+  expandCT:boolean = false;
+
+  members = [
+    {
+      name: 'John',
+      description: 'Frontend Developer'
+    },
+    {
+      name: 'Jane',
+      description: 'Backend Developer'
+    },
+    {
+      name: 'Bob',
+      description: 'UI/UX Designer'
+    }
+  ]
 
 }
